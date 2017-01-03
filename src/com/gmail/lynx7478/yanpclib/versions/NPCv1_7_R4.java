1f22039c8808ca70fbd8f5d1abe2b2d6fedf02ad
SKA4 <lynx7478@gmail.com>
2017-01-03 19:26:15 +0100

    Added 1.7.X support.

---
+++ src/com/gmail/lynx7478/yanpclib/versions/NPCv1_7_R4.java
@@ -0,0 +1,66 @@
+package com.gmail.lynx7478.yanpclib.versions;
+
+import java.util.UUID;
+
+import net.minecraft.server.v1_7_R4.EntityPlayer;
+import net.minecraft.server.v1_7_R4.MinecraftServer;
+import net.minecraft.server.v1_7_R4.PacketPlayOutEntityDestroy;
+import net.minecraft.server.v1_7_R4.PacketPlayOutNamedEntitySpawn;
+import net.minecraft.server.v1_7_R4.PlayerConnection;
+import net.minecraft.server.v1_7_R4.PlayerInteractManager;
+import net.minecraft.server.v1_7_R4.WorldServer;
+import net.minecraft.util.com.mojang.authlib.GameProfile;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.craftbukkit.v1_7_R4.CraftServer;
+import org.bukkit.craftbukkit.v1_7_R4.CraftWorld;
+import org.bukkit.craftbukkit.v1_7_R4.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+
+import com.gmail.lynx7478.yanpclib.YANPCHandler;
+
+public class NPCv1_7_R4 implements YANPCHandler {
+
+    private EntityPlayer entity;
+
+    // Constructor.
+    public NPCv1_7_R4()
+    {
+    	//TODO:
+    	// We should probably make the NPC here, still haven't figured out how without breaking the whole thing.
+    	// Any ideas?
+    }
+
+    public EntityPlayer getEntity()
+    {
+        return entity;
+    }
+
+	@Override
+	public void spawn(Location loc, String name) 
+	{
+        MinecraftServer nmsServer = ((CraftServer) Bukkit.getServer()).getServer();
+        WorldServer nmsWorld = ((CraftWorld) loc.getWorld()).getHandle();
+        entity = new EntityPlayer(nmsServer, nmsWorld, new GameProfile(UUID.randomUUID().toString(), name), new PlayerInteractManager(nmsWorld));
+        entity.setLocation(loc.getX(), loc.getY(), loc.getZ(), loc.getYaw(), loc.getPitch());
+        for(Player p : Bukkit.getOnlinePlayers())
+        {
+            PlayerConnection connection = ((CraftPlayer) p).getHandle().playerConnection;
+            connection.sendPacket(new PacketPlayOutNamedEntitySpawn(entity));
+        }
+		
+	}
+
+	@Override
+	public void destroy() 
+	{
+        for(Player p : Bukkit.getOnlinePlayers())
+        {
+            PlayerConnection connection = ((CraftPlayer) p).getHandle().playerConnection;
+            connection.sendPacket(new PacketPlayOutEntityDestroy(entity.getId()));
+            entity = null;
+        }
+	}
+
+}
